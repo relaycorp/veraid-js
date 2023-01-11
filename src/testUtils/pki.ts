235ee990f71b54201f289997d8bb5e7590736c2a
Gus Narea <gnarea@users.noreply.github.com>
2023-01-11 19:18:32 +0000

    groundwork

--- src/testUtils/pki.ts
+++ src/testUtils/pki.ts
@@ -2,20 +2,13 @@ import Certificate from '../lib/utils/x509/Certificate.js';
 import { generateRsaKeyPair, getPublicKeyDigestHex } from '../lib/utils/keys.js';
 import type FullIssuanceOptions from '../lib/utils/x509/FullIssuanceOptions.js';
 
+import { reSerializeCertificate } from './pkijs.js';
+
 interface StubCertConfig {
   readonly attributes: Partial<FullIssuanceOptions>;
   readonly issuerCertificate: Certificate;
   readonly issuerPrivateKey: CryptoKey;
   readonly subjectPublicKey: CryptoKey;
-}
-
-function reSerializeCertificate(cert: Certificate): Certificate {
-  // TODO: Raise bug in PKI.js project
-  // PKI.js sometimes tries to use attributes that are only set *after* the certificate has been
-  // deserialized, so you'd get a TypeError if you use a certificate you just created in memory.
-  // For example, `extension.parsedValue` would be `undefined` in
-  // https://github.com/PeculiarVentures/PKI.js/blob/9a39551aa9f1445406f96680318014c8d714e8e3/src/CertificateChainValidationEngine.js#L155
-  return Certificate.deserialize(cert.serialize());
 }
 
 /**
