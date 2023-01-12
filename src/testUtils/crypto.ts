3c33a9dd097974345b37a893418b17b033ed9c99
Gus Narea <gnarea@users.noreply.github.com>
2023-01-12 15:42:50 +0000

    refactor

--- src/testUtils/crypto.ts
+++ src/testUtils/crypto.ts
@@ -3,7 +3,3 @@ import { createHash } from 'node:crypto';
 export function calculateDigest(algorithm: string, plaintext: ArrayBuffer | Buffer): Buffer {
   return createHash(algorithm).update(Buffer.from(plaintext)).digest();
 }
-
-export function sha256Hex(plaintext: ArrayBuffer | Buffer): string {
-  return calculateDigest('sha256', plaintext).toString('hex');
-}
