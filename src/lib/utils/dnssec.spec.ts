2ac12d0a6bdcb362808a575390f390d8db8bbea6
Gus Narea <gnarea@users.noreply.github.com>
2023-01-12 14:26:32 +0000

    impl

--- src/lib/utils/dnssec.spec.ts
+++ src/lib/utils/dnssec.spec.ts
@@ -1,16 +1,22 @@
 import { jest } from '@jest/globals';
-import { Question } from '@relaycorp/dnssec';
+import { DnsRecord, Message } from '@relaycorp/dnssec';
 import { DNSoverHTTPS } from 'dohdec';
 
-import { dnssecOnlineResolve } from './dnssec.js';
+import { arrayBufferFrom } from '../../testUtils/buffers.js';
+import VeraError from '../VeraError.js';
+import { ORG_NAME } from '../../testUtils/veraStubs.js';
 
-const STUB_QUESTION = new Question('example.com', 'A');
-const STUB_DNS_RESPONSE = Buffer.from('The DNS response');
+import { dnssecOnlineResolve, makeDnssecOfflineResolver } from './dnssec.js';
+
+const STUB_RECORD = new DnsRecord(ORG_NAME, 'TXT', 'IN', 42, 'foo');
+const STUB_QUESTION = STUB_RECORD.makeQuestion();
+const STUB_RESPONSE = new Message({ rcode: 0 }, [STUB_QUESTION], [STUB_RECORD]);
+const STUB_DNS_RESPONSE_SERIALISED = STUB_RESPONSE.serialise();
 
 describe('dnssecOnlineResolve', () => {
   const mockDohLookup = jest.spyOn(DNSoverHTTPS.prototype, 'lookup');
   beforeEach(() => {
-    mockDohLookup.mockReset().mockResolvedValue(STUB_DNS_RESPONSE);
+    mockDohLookup.mockReset().mockResolvedValue(STUB_DNS_RESPONSE_SERIALISED);
   });
 
   test('Specified question should be queried', async () => {
@@ -63,6 +69,37 @@ describe('dnssecOnlineResolve', () => {
   test('Response should be returned', async () => {
     const response = await dnssecOnlineResolve(STUB_QUESTION);
 
-    expect(response).toBe(STUB_DNS_RESPONSE);
+    expect(response).toBe(STUB_DNS_RESPONSE_SERIALISED);
+  });
+});
+
+describe('makeDnssecOfflineResolver', () => {
+  test('Malformed responses should be refused', () => {
+    const malformedResponse = arrayBufferFrom('malformed');
+
+    expect(() => makeDnssecOfflineResolver([malformedResponse])).toThrowWithMessage(
+      VeraError,
+      'At least one of the response messages is malformed',
+    );
+  });
+
+  describe('Resolver', () => {
+    test('Existing response should be returned', async () => {
+      const resolver = makeDnssecOfflineResolver([arrayBufferFrom(STUB_DNS_RESPONSE_SERIALISED)]);
+
+      const response = (await resolver(STUB_QUESTION)) as Message;
+      expect(Buffer.from(response.serialise())).toStrictEqual(
+        Buffer.from(STUB_DNS_RESPONSE_SERIALISED),
+      );
+    });
+
+    test('Missing response should result in error', async () => {
+      const resolver = makeDnssecOfflineResolver([]);
+
+      await expect(resolver(STUB_QUESTION)).rejects.toThrowWithMessage(
+        VeraError,
+        `Could not find response for ${STUB_QUESTION.key}`,
+      );
+    });
   });
 });
--- src/lib/utils/dnssec.ts
+++ src/lib/utils/dnssec.ts
@@ -1,5 +1,7 @@
-import { type Question } from '@relaycorp/dnssec';
+import { Message, type Question, type Resolver } from '@relaycorp/dnssec';
 import { DNSoverHTTPS } from 'dohdec';
+
+import VeraError from '../VeraError.js';
 
 const DOH = new DNSoverHTTPS({ url: 'https://cloudflare-dns.com/dns-query' });
 
@@ -12,4 +14,24 @@ export async function dnssecOnlineResolve(question: Question): Promise<Buffer> {
     rrtype: question.getTypeName(),
   });
   return response as Buffer;
+}
+
+export function makeDnssecOfflineResolver(responsesSerialised: readonly ArrayBuffer[]): Resolver {
+  const responses = responsesSerialised.map((responseSerialised) => {
+    let response: Message;
+    try {
+      response = Message.deserialise(Buffer.from(responseSerialised));
+    } catch (err) {
+      throw new VeraError('At least one of the response messages is malformed', { cause: err });
+    }
+    return response;
+  });
+  // eslint-disable-next-line @typescript-eslint/require-await
+  return async (question) => {
+    const matchingResponse = responses.find((response) => response.answersQuestion(question));
+    if (!matchingResponse) {
+      throw new VeraError(`Could not find response for ${question.key}`);
+    }
+    return matchingResponse;
+  };
 }
