3d435ca55acf8a15f296dd45fada4d1dc6a79c5d
Gus Narea <gnarea@users.noreply.github.com>
2023-01-12 17:02:50 +0000

    impl

--- src/lib/dns/rdataSerialisation.ts
+++ src/lib/dns/rdataSerialisation.ts
@@ -6,19 +6,36 @@ import { getPkijsCrypto } from '../utils/pkijs.js';
 import { KeyIdType } from '../KeyIdType.js';
 
 import { type RdataGenerationOptions } from './RdataGenerationOptions.js';
+import { type VeraRdataFields } from './VeraRdataFields.js';
+import { KeyAlgorithmType } from './KeyAlgorithmType.js';
 
 const CRYPTO_ENGINE = getPkijsCrypto();
 
+const FIELDS_REGEX = /^\s*(?<fields>\S.+\S)\s*$/u;
+const FIELD_SEPARATOR_REGEX = /\s+/u;
+
 const MAX_TTL_OVERRIDE_DAYS = 90;
 const MAX_TTL_OVERRIDE_SECONDS = secondsInDay * MAX_TTL_OVERRIDE_DAYS;
+const TTL_OVERRIDE_REGEX = /^\d+$/u;
 
-const ALGORITHM_ID_BY_RSA_MODULUS: { readonly [modulus: number]: number } = {
+const MIN_RDATA_FIELDS = 4;
+
+const ALGORITHM_ID_BY_RSA_MODULUS: { readonly [modulus: number]: KeyAlgorithmType } = {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  2048: 0,
+  2048: KeyAlgorithmType.RSA_2048,
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  3072: 1,
+  3072: KeyAlgorithmType.RSA_3072,
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  4096: 2,
+  4096: KeyAlgorithmType.RSA_4096,
+};
+
+const ALGORITHM_ID_BY_STRING: { readonly [id: string]: KeyAlgorithmType } = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  '1': KeyAlgorithmType.RSA_2048,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  '2': KeyAlgorithmType.RSA_3072,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  '3': KeyAlgorithmType.RSA_4096,
 };
 
 const HASH_BY_KEY_ID_TYPE = {
@@ -27,7 +44,16 @@ const HASH_BY_KEY_ID_TYPE = {
   [KeyIdType.SHA512]: 'SHA-512',
 };
 
-function getAlgorithmForKey(key: CryptoKey): number {
+const KEY_ID_TYPE_BY_STRING: { readonly [type: string]: KeyIdType } = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  '1': KeyIdType.SHA256,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  '2': KeyIdType.SHA384,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  '3': KeyIdType.SHA512,
+};
+
+function getAlgorithmIdForKey(key: CryptoKey): number {
   if (key.algorithm.name !== 'RSA-PSS') {
     throw new VeraError(`Only RSA-PSS keys are supported (got ${key.algorithm.name})`);
   }
@@ -36,6 +62,22 @@ function getAlgorithmForKey(key: CryptoKey): number {
     throw new VeraError(`RSA key with modulus ${modulusLength} is unsupported`);
   }
   return ALGORITHM_ID_BY_RSA_MODULUS[modulusLength];
+}
+
+function getAlgorithmId(algorithmString: string): KeyAlgorithmType {
+  const id = ALGORITHM_ID_BY_STRING[algorithmString] as KeyAlgorithmType | undefined;
+  if (!id) {
+    throw new VeraError(`Unknown algorithm id ("${algorithmString}")`);
+  }
+  return id;
+}
+
+function getKeyIdTypeFromString(keyIdTypeString: string): KeyIdType {
+  const type = KEY_ID_TYPE_BY_STRING[keyIdTypeString] as KeyIdType | undefined;
+  if (!type) {
+    throw new VeraError(`Unknown key id type ("${keyIdTypeString}")`);
+  }
+  return type;
 }
 
 async function getKeyId(orgPublicKey: CryptoKey, keyIdType: KeyIdType): Promise<string> {
@@ -59,12 +101,19 @@ function validateTtlOverride(ttlOverride: number): void {
   }
 }
 
+function getTtlOverrideFromString(ttlOverrideString: string): number {
+  if (!TTL_OVERRIDE_REGEX.test(ttlOverrideString)) {
+    throw new VeraError(`Malformed TTL override ("${ttlOverrideString}")`);
+  }
+  return Number.parseInt(ttlOverrideString, 10);
+}
+
 export async function generateTxtRdata(
   orgPublicKey: CryptoKey,
   ttlOverride: number,
   options: Partial<RdataGenerationOptions> = {},
 ): Promise<string> {
-  const algorithm = getAlgorithmForKey(orgPublicKey);
+  const algorithm = getAlgorithmIdForKey(orgPublicKey);
   const keyIdType = options.keyIdType ?? KeyIdType.SHA256;
   const keyId = await getKeyId(orgPublicKey, keyIdType);
   validateTtlOverride(ttlOverride);
@@ -76,4 +125,20 @@ export async function generateTxtRdata(
     ...(options.serviceOid === undefined ? [] : [options.serviceOid]),
   ];
   return fields.join(' ');
+}
+
+export function parseTxtRdata(rdata: string): VeraRdataFields {
+  const rdataSanitised = rdata.replace(FIELDS_REGEX, '$<fields>');
+  const fields = rdataSanitised.split(FIELD_SEPARATOR_REGEX);
+  if (fields.length < MIN_RDATA_FIELDS) {
+    throw new VeraError(
+      `RDATA should have at least 4 space-separated fields (got ${fields.length})`,
+    );
+  }
+
+  const [algorithmString, keyIdTypeString, keyId, ttlOverrideString, serviceOid] = fields;
+  const algorithm = getAlgorithmId(algorithmString);
+  const keyIdType = getKeyIdTypeFromString(keyIdTypeString);
+  const ttlOverride = getTtlOverrideFromString(ttlOverrideString);
+  return { algorithm, keyId, keyIdType, ttlOverride, serviceOid };
 }
