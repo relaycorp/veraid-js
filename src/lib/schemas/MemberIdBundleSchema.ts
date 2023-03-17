25fbddb809b3ba015c93a594f45cb726fec648e5
Gus Narea <gnarea@users.noreply.github.com>
2023-03-17 23:23:38 +0100

    fix: Serialise heterogeneous `SEQUENCE`s with implicitly-tagged items (#103)
    Found whilst checking compatibility with JVM implementation: https://github.com/relaycorp/veraid-jvm/issues/14

--- src/lib/schemas/MemberIdBundleSchema.ts
+++ src/lib/schemas/MemberIdBundleSchema.ts
@@ -6,15 +6,15 @@ import { Certificate } from '@peculiar/asn1-x509';
 import { DnssecChainSchema } from './DnssecChainSchema.js';
 
 export class MemberIdBundleSchema {
-  @AsnProp({ type: AsnPropTypes.Integer })
+  @AsnProp({ type: AsnPropTypes.Integer, context: 0, implicit: true })
   public version!: number;
 
-  @AsnProp({ type: DnssecChainSchema })
+  @AsnProp({ type: DnssecChainSchema, context: 1, implicit: true })
   public dnssecChain!: DnssecChainSchema;
 
-  @AsnProp({ type: Certificate })
+  @AsnProp({ type: Certificate, context: 2, implicit: true })
   public organisationCertificate!: Certificate;
 
-  @AsnProp({ type: Certificate })
+  @AsnProp({ type: Certificate, context: 3, implicit: true })
   public memberCertificate!: Certificate;
 }
--- src/lib/schemas/SignatureMetadataSchema.ts
+++ src/lib/schemas/SignatureMetadataSchema.ts
@@ -5,9 +5,9 @@ import { AsnProp, AsnPropTypes } from '@peculiar/asn1-schema';
 import { DatePeriodSchema } from './DatePeriodSchema.js';
 
 export class SignatureMetadataSchema {
-  @AsnProp({ type: AsnPropTypes.ObjectIdentifier })
+  @AsnProp({ type: AsnPropTypes.ObjectIdentifier, context: 0, implicit: true })
   public serviceOid!: string;
 
-  @AsnProp({ type: DatePeriodSchema })
+  @AsnProp({ type: DatePeriodSchema, context: 1, implicit: true })
   public validityPeriod!: DatePeriodSchema;
 }
