92719618cff06aa72f4740494ba6dbb401e8552e
Gus Narea <gnarea@users.noreply.github.com>
2023-03-17 23:11:37 +0100

    fix: Serialise heterogeneous `SEQUENCE`s with implicitly-tagged items
    Found whilst checking compatibility with JVM implementation: https://github.com/relaycorp/veraid-jvm/issues/14

--- src/lib/schemas/DatePeriodSchema.ts
+++ src/lib/schemas/DatePeriodSchema.ts
@@ -2,9 +2,9 @@
 import { AsnProp, AsnPropTypes } from '@peculiar/asn1-schema';
 
 export class DatePeriodSchema {
-  @AsnProp({ type: AsnPropTypes.GeneralizedTime })
+  @AsnProp({ type: AsnPropTypes.GeneralizedTime, context: 0, implicit: true })
   public start!: Date;
 
-  @AsnProp({ type: AsnPropTypes.GeneralizedTime })
+  @AsnProp({ type: AsnPropTypes.GeneralizedTime, context: 1, implicit: true })
   public end!: Date;
 }
--- src/lib/schemas/SignatureBundleSchema.ts
+++ src/lib/schemas/SignatureBundleSchema.ts
@@ -7,15 +7,15 @@ import { Certificate } from '@peculiar/asn1-x509';
 import { DnssecChainSchema } from './DnssecChainSchema.js';
 
 export class SignatureBundleSchema {
-  @AsnProp({ type: AsnPropTypes.Integer })
+  @AsnProp({ type: AsnPropTypes.Integer, context: 0, implicit: true })
   public version!: number;
 
-  @AsnProp({ type: DnssecChainSchema })
+  @AsnProp({ type: DnssecChainSchema, context: 1, implicit: true })
   public dnssecChain!: DnssecChainSchema;
 
-  @AsnProp({ type: Certificate })
+  @AsnProp({ type: Certificate, context: 2, implicit: true })
   public organisationCertificate!: Certificate;
 
-  @AsnProp({ type: ContentInfo })
+  @AsnProp({ type: ContentInfo, context: 3, implicit: true })
   public signature!: ContentInfo;
 }
