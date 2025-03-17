88af3fc0bb6c456bc1b55540a5b43b8663731ce7
Gus Narea <gnarea@users.noreply.github.com>
2025-03-17 14:17:10 +0000

    Reorganise `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 /* eslint-disable import/no-unused-modules */
 
+// DNS handling
+export { generateTxtRdata } from './lib/dns/rdataSerialisation.js';
 export { type DnsResolutionOptions, VeraidDnssecChain } from './lib/dns/VeraidDnssecChain.js';
-export { issueMemberCertificate } from './lib/pki/member.js';
+
+// X.509 handling
 export { selfIssueOrganisationCertificate } from './lib/pki/organisation.js';
+export type { OrganisationSigner } from './lib/OrganisationSigner.js';
+export { issueMemberCertificate } from './lib/pki/member.js';
+export type { Member } from './lib/Member.js';
+export { validateUserName } from './lib/idValidation.js';
+export { MemberIdBundle } from './lib/memberIdBundle/MemberIdBundle.js';
 export type { CertificateIssuanceOptions } from './lib/pki/CertificateIssuanceOptions.js';
 export { Certificate } from './lib/utils/x509/Certificate.js';
-export { generateTxtRdata } from './lib/dns/rdataSerialisation.js';
-export { MemberIdBundle } from './lib/memberIdBundle/MemberIdBundle.js';
-export { SignatureBundle } from './lib/SignatureBundle.js';
+
+// Signature handling
 export type { SignatureOptions } from './lib/SignatureOptions.js';
-export type { OrganisationSigner } from './lib/OrganisationSigner.js';
-export type { Member } from './lib/Member.js';
 export type { SignatureBundleVerification } from './lib/SignatureBundleVerification.js';
 export type { IDatePeriod } from './lib/dates.js';
-export { validateUserName } from './lib/idValidation.js';
+export { SignatureBundle } from './lib/SignatureBundle.js';
