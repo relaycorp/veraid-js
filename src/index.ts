1696efe5403f5049f2173bed84b5a787f6b83a6f
Gus Narea <gnarea@users.noreply.github.com>
2025-04-04 13:22:16 +0100

    fix: Export `Chain` class

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,14 @@ export { type DnsResolutionOptions, VeraidDnssecChain } from './lib/dns/VeraidDn
 
 // X.509 handling
 export { selfIssueOrganisationCertificate } from './lib/pki/organisation.js';
-export { OrganisationSigner } from './lib/OrganisationSigner.js';
 export { issueMemberCertificate } from './lib/pki/member.js';
 export type { Member } from './lib/Member.js';
 export { validateUserName } from './lib/idValidation.js';
-export { MemberIdBundle } from './lib/memberIdBundle/MemberIdBundle.js';
 export type { CertificateIssuanceOptions } from './lib/pki/CertificateIssuanceOptions.js';
 export { Certificate } from './lib/utils/x509/Certificate.js';
+export { Chain } from './lib/Chain.js';
+export { MemberIdBundle } from './lib/memberIdBundle/MemberIdBundle.js';
+export { OrganisationSigner } from './lib/OrganisationSigner.js';
 
 // Signature handling
 export type { SignatureOptions } from './lib/SignatureOptions.js';
